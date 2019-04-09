85b30476b19eaa2c21ef02af32db72ffe830444e
Wilhelm Duembeg <wilhelm.duembeg@dura-machines.com>
2019-04-08 23:48:56 -0700

    Ensure USB enumeration only happens on DFU build
    * Ensure USB enumeration only happens in DFU builds, also add comments
    * Configure for SWD and JTAG disable. before it fully deisable JTAG why?

--- grbl/main.c
+++ grbl/main.c
@@ -51,25 +51,25 @@ void USART1_Configuration(u32 BaudRate)
 	USART_InitTypeDef USART_InitStructure;
 	NVIC_InitTypeDef NVIC_InitStructure;
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
-	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;   
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;  
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
 
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; 
-	NVIC_Init(&NVIC_InitStructure);                 
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
 
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	USART_InitStructure.USART_BaudRate = BaudRate;	  
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b; 
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;	 
-	USART_InitStructure.USART_Parity = USART_Parity_No;	 
+	USART_InitStructure.USART_BaudRate = BaudRate;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 	USART1->CR1 |= (USART_CR1_RE | USART_CR1_TE);
@@ -90,7 +90,7 @@ int main(void)
 #endif
 {
 #if defined (STM32F103C8)
-    GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE);
+    GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
 #ifdef LEDBLINK
 	GPIO_InitTypeDef GPIO_InitStructure;
 
@@ -101,6 +101,8 @@ int main(void)
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 #endif
 
+// trigger host to do usb enumeration for this device
+// needed to switch from enumeration done in boot loader
 #ifdef MAPLE_MINI
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
@@ -108,13 +110,14 @@ int main(void)
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
   GPIO_Init(GPIOB, &GPIO_InitStructure);
   GPIO_WriteBit(GPIOB, GPIO_Pin_9, 1);
-#else
+#elif defined(WITH_DFU_BOOTLOADER)
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
   GPIO_WriteBit(GPIOA, GPIO_Pin_12, 0);
+  delay_ms(5);
 #endif
 
 	//Set_System();
@@ -126,6 +129,7 @@ int main(void)
 	USB_Init();
 #endif
 
+// after hot usb enumeration, let usb function normally
 #ifdef MAPLE_MINI
   GPIO_WriteBit(GPIOB, GPIO_Pin_9, 0);
 #endif
@@ -157,7 +161,7 @@ int main(void)
   #else
     sys.state = STATE_IDLE;
   #endif
-  
+
   // Check for power-up and set system alarm if homing is enabled to force homing cycle
   // by setting Grbl's alarm state. Alarm locks out all g-code commands, including the
   // startup scripts, but allows access to settings and internal commands. Only a homing
